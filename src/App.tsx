fff9dcbec2ba04abed6ea95e93c5bd54ad0e8929
HerveMashukane <hervemashukane@gmail.com>
2026-04-03 17:18:17 +0200

    global styles definition in tailwind.config.js file for brand consistancy accross the platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import Navbar from './components/layout/Navbar';
 
 function App() {
 
   return (
-    <h1 className="text-5xl text-red-500 font-bold">
-      Tailwind is finally working 🚀
-    </h1>
+    <Navbar />
   )
 }
 
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,6 +3,18 @@ import { useState } from "react";
 export default function Navbar() {
 
   return (
-    <h1 className="text-red-800">Hello World!</h1>
+    <nav>
+      <div className="bg-brand-background text-brand-primary font-base">
+  Hello
+</div>
+
+<button className="bg-brand-primary hover:bg-brand-primary-hover text-white py-btn-y px-btn-x rounded-btn">
+  Primary Button
+</button>
+
+<button className="bg-brand-secondary hover:bg-brand-secondary-hover text-white py-btn-y px-btn-x rounded-btn">
+  Secondary Button
+</button>
+    </nav>
   );
 }
